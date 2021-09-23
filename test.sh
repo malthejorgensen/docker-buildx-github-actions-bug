ab3e7f4bbe911315f4a23cdaa8d1594d26fa4aae
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2021-09-23 17:01:11 +0200

    Add `--iidfile` and `--metadata-file` to match Github Actions behavior

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build -t malthe-prod-base -f Dockerfile.base .
-docker buildx build -t malthe-prod -f Dockerfile.prod .
+docker buildx build --iidfile tmp/iidfile-1 --metadata-file tmp/metadata-file-1 -t malthe-prod-base -f Dockerfile.base .
+docker buildx build --iidfile tmp/iidfile-2 --metadata-file tmp/metadata-file-2 -t malthe-prod -f Dockerfile.prod .
 docker run --rm malthe-prod
